7734ed079ae86e7db6f0f95df8965361a7b1d3fc
Jerin Lee <taurenjerin@qq.com>
2021-02-18 12:20:16 +0800

    add lost files.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

---
+++ src/common/HyperLink.ts
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+export default class HyperLink {
+    text: string;
+    title: string;
+    rel: string;
+    uri: string;
+
+    constructor(text: string, title: string, rel: string, uri: string) {
+        this.text = text;
+        this.title = title;
+        this.rel = rel;
+        this.uri = uri;
+    }
+
+    static create(text: string, uri: string): HyperLink {
+        const r = new HyperLink(text, text, undefined, uri);
+        return r;
+    }
+
+    clone() {
+        const r = new HyperLink(this.text, this.title, this.rel, this.uri);
+        return r;
+    }
+    
+}
---
+++ src/common/ImageLink.ts
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+export default class ImageLink {
+    title: string;
+    altText: string;
+    uri: string;
+
+    constructor(title: string, altText: string, uri: string) {
+        this.title = title;
+        this.altText = altText;
+        this.uri = uri;
+    }
+
+    static create(title: string, uri: string): ImageLink {
+        const r = new ImageLink(title, title, uri);
+        return r;
+    }
+
+    clone(): ImageLink {
+        const r = new ImageLink(this.title, this.altText, this.uri);
+        return r;
+    }
+    
+}
